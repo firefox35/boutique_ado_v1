375feaf9d63c645cf87345ceddf95c110289b30a
firefox35 <f1firefox@yahoo.ie>
2024-01-11 20:06:46 +0000

    Added sorting and counts to product page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
+
 from .models import Product, Category
 
 # Create your views here.
@@ -22,7 +25,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -41,16 +45,17 @@ def all_products(request):
                     request, "You didn't enter any search criterial!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
     context = {
-            'products': products,
-            'search_term': query,
-            'current_categories': categories,
-            'current_sorting': current_sorting,
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
