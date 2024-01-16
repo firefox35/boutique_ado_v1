35fe7e9648bb6a2ca7bab74478c5d3be3ba55906
firefox35 <f1firefox@yahoo.ie>
2024-01-16 20:45:13 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,10 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
             'order_form': order_form,
+            'stripe_public_key': 'pk_test_51OYvEXEhOjsBBU1Hyd6YydpoCJQdABfKQ4gYMJqocP2Q4zG7nviXt161QHs51x4iu4NfuMQmFbaU6doKzyPWduId00MNcaWsM3',
+            'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
+
+    
